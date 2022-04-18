3c910d41e0205e17db6abcbdb727f18136b404d5
Bittersweet-girl <enatav1@gmail.com>
2022-04-18 22:15:15 +0300

    feat: add create modal function

--- shelter/assets/js/modal.js
+++ shelter/assets/js/modal.js
@@ -1,11 +1,14 @@
+import { pets } from "./pets.js";
 export function Modal() {
   const openModal = document.querySelector(".pagination__items");
   const modal = document.querySelector(".modal-background");
   const closeModal = document.querySelector(".modal__close");
 
-  openModal.addEventListener('click', (e) => {
-    if (e.target.classList.contains('pagination__item')) {
-      modal.classList.add('modal_open');
+  openModal.addEventListener("click", (e) => {
+    if (e.target.closest(".pagination__item")) {
+      console.log(e.target.querySelector("h2"));
+      createModal();
+      modal.classList.add("modal_open");
     }
   });
 
@@ -13,9 +16,29 @@ export function Modal() {
     modal.classList.remove("modal_open");
   });
 
-  window.addEventListener('click', (e) => {
+  window.addEventListener("click", (e) => {
     if (e.target === modal) {
       modal.classList.remove("modal_open");
     }
-  })
+  });
+  
+  function createModal() {
+    let i = 1;
+    const modalContent = document.querySelector(".modal-text");
+    const modalImage = document.querySelector(".modal-img");
+    modalImage.innerHTML = "";
+    modalImage.innerHTML += `<img src="${pets[i].img}" alt="pet">`;
+    modalContent.innerHTML = "";
+    modalContent.innerHTML += `<h2>${pets[i].name}</h2>
+            <h3>${pets[i].type} - ${pets[i].breed}</h3>
+            <p>${pets[i].description}</p>
+            <ul class="modal-list">
+              <li><span><b>Age:</b> ${pets[i].age}</span></li>
+              <li><span><b>Inoculations:</b> ${pets[i].inoculations}</span></li>
+              <li><span><b>Diseases:</b> ${pets[i].diseases}</span></li>
+              <li><span><b>Parasites:</b> ${pets[i].parasites}</span></li>
+            </ul>`;
+    
 }
+}
+
