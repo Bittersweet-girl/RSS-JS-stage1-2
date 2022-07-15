4338cc2039c3543df49edeb3c3dc30e6a7283b0b
Bittersweet-girl <enatav1@gmail.com>
2022-07-15 21:13:00 +0300

    feat: add layout to filtr and search

--- online-shop/src/index.ts
+++ online-shop/src/index.ts
@@ -4,3 +4,23 @@ import './style.css';
 import './assets/img/img 1.jpg';
 import './assets/img/img 2.jpg';
 import './assets/img/img 3.jpg';
+
+const sliders = document.querySelectorAll<HTMLInputElement>('input[type="range"]');
+
+sliders[0].addEventListener('input', () => {
+    if (sliders[0].value > sliders[1].value) {
+        sliders[1].value = sliders[0].value;
+    }
+});
+
+sliders[1].addEventListener('input', () => {
+    if (sliders[1].value < sliders[0].value) {
+        sliders[0].value = sliders[1].value;
+    }
+});
+
+sliders.forEach((slider) => {
+    slider.addEventListener('change', () => {
+        console.log(`from ${sliders[0].value} to ${sliders[1].value}`);
+    });
+});
