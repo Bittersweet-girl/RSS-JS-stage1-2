3122b71fcc535933444d226fde6336ea8dc9e372
Bittersweet-girl <enatav1@gmail.com>
2022-07-16 19:12:28 +0300

    feat: add items to DB

--- online-shop/src/index.ts
+++ online-shop/src/index.ts
@@ -6,6 +6,13 @@ import './assets/img/img 2.jpg';
 import './assets/img/img 3.jpg';
 import './assets/img/img 4.jpg';
 import './assets/img/img 5.jpg';
+import './assets/img/img 6.jpg';
+import './assets/img/img 7.jpeg';
+import './assets/img/img 8.jpg';
+import './assets/img/img 9.jpg';
+import './assets/img/img 10.jpg';
+import './assets/img/img 11.jpg';
+import './assets/img/img 12.jpg';
 
 const sliders = document.querySelectorAll<HTMLInputElement>('input[type="range"]');
 
@@ -28,7 +35,6 @@ sliders.forEach((slider) => {
 });
 
 import { App } from './app/app';
-
 const app = new App();
 app.render();
 
