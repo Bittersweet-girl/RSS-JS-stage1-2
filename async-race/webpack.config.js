68af44081cc003fb9612dc85a05c3ba0acb52a62
Bittersweet-girl <enatav1@gmail.com>
2022-07-29 01:17:09 +0300

    init: start async-race task & config webpack

---
+++ async-race/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const EslingPlugin = require("eslint-webpack-plugin");
+
+
+const baseConfig = {
+  entry: path.resolve(__dirname, "./src/index"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.[tj]s$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./src/index.html"),
+      filename: "index.html",
+    }),
+    new EslingPlugin({ extensions: "ts" }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CopyPlugin({
+      patterns: [{ from: "src/assets", to: "./dist/assets" }],
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === "prod";
+  const envConfig = isProductionMode
+    ? require("./webpack.prod.config")
+    : require("./webpack.dev.config");
+
+  return merge(baseConfig, envConfig);
+};
