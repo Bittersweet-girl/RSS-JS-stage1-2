e51f0e9eaf110ebc684b5d39281af532a30d0d60
Bittersweet-girl <enatav1@gmail.com>
2022-08-02 19:39:29 +0300

    feat: add garage title and pagination buttons

--- async-race/src/mainlayout/header.ts
+++ async-race/src/mainlayout/header.ts
@@ -3,6 +3,7 @@ import './header.scss';
 export default function makeHeader(): string {
   return `<header class="header">
               <a href="#/" class="header__link">Garage</a>
+              <h1>Acync Race</h1>
               <a href="#/winners" class="header__link">Winners</a>
           </header>`;
 }
