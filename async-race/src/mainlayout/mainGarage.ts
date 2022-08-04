2951e6ef191ccf8e0bd80a373705ee95e818c152
Bittersweet-girl <enatav1@gmail.com>
2022-08-04 18:56:40 +0300

    fix: change buttons style

--- async-race/src/mainlayout/mainGarage.ts
+++ async-race/src/mainlayout/mainGarage.ts
@@ -3,22 +3,24 @@ import { renderGarageResult } from '../garage';
 
 export function makeMainGarage(): string {
   return `<section class="garage">
-    <div class="garage-inputs">
-        <div class="garage-inputs__create">
-            <input class="garage-create-text" type="text">
-            <input class="garage-create-color" type="color" value="#ffffff">
-            <button class="btn garage-create-button">create</button>
-        </div>
-        <div class="garage-inputs__update">
-            <input class="garage-update-text" type="text">
-            <input class="garage-update-color" type="color" value="#ffffff">
-            <button class="btn garage-update-button" disabled="">update</button>
-        </div>
-    </div>
-    <div class="garage__buttons">
-        <button class="btn garage-race-btn">Race</button>
-        <button class="btn garage-reset-btn">Reset</button>
-        <button class="btn garage-generate-btn">Generate cars</button>
+    <div class="garage-options">
+      <div class="garage-inputs">
+          <div class="garage-inputs__create">
+              <input class="garage-create-text" type="text">
+              <input class="garage-create-color" type="color" value="#ffffff">
+              <button class="btn garage-create-button">create</button>
+          </div>
+          <div class="garage-inputs__update">
+              <input class="garage-update-text" type="text">
+              <input class="garage-update-color" type="color" value="#ffffff">
+              <button class="btn garage-update-button" disabled="">update</button>
+          </div>
+      </div>
+      <div class="garage__buttons">
+          <button class="btn garage-race-btn">Race</button>
+          <button class="btn garage-reset-btn">Reset</button>
+          <button class="btn garage-generate-btn">Generate cars</button>
+      </div>
     </div>
     <div class="garage-content">
       ${renderGarageResult()}
